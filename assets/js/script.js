37d6afd417dbd5949bd841a30ed6cbb77793e31e
Colin Wu <colin.y.wu@gmail.com>
2019-12-26 23:58:14 -0500

    Pure HTML/CSS Resume

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(
- function () {
-     alert($('p').text())
- }
-)
-
+  //document doesn't need quotation because it's object.
+  function() {
+    alert($("p").text()); //needs quotation because it's a tag.
+  }
+);
